585214a96aafcc4346e3b4731a8ae766a05fa111
laccces <en07djc@icloud.com>
2023-05-26 09:16:58 +0100

    Added templates for common requests

--- asana-export.py
+++ asana-export.py
@@ -1,5 +1,42 @@
 import asana
+import requests 
 
-client = asana.Client.access_token('1/1201719304365594:93eb946e8425cb45865ad7829a527f0c')
+client = asana.Client.access_token('PERSONAL_ACCESS_TOKEN')
 
 result = client.projects.get_projects({'param': 'value', 'param': 'value'}, opt_pretty=True)
+
+
+# import multiple tasks
+import asana
+
+client = asana.Client.access_token('PERSONAL_ACCESS_TOKEN')
+
+result = client.tasks.get_tasks({'param': 'value', 'param': 'value'}, opt_pretty=True)
+
+# get a time entry ID
+import requests
+
+url = "https://app.asana.com/api/1.0/tasks/1204021827439397/time_tracking_entries"
+
+headers = {
+    "accept": "application/json",
+    "authorization": "Bearer PERSONAL_ACCESS_TOKEN"
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.text)
+
+# Get a time tracking entry
+import requests
+
+url = "https://app.asana.com/api/1.0/time_tracking_entries/1204021827439402"
+
+headers = {
+    "accept": "application/json",
+    "authorization": "Bearer PERSONAL_ACCESS_TOKEN"
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.text)
