533615ce296f05833e2c05bf416a4e3c32892af3
laccces <en07djc@icloud.com>
2023-05-26 09:13:05 +0100

    venv created

--- asana-export.py
+++ asana-export.py
@@ -1,5 +1,5 @@
 import asana
 
-client = asana.Client.access_token('1/1201719304365594:39ca3917c86bb065b217dfe234e4731c')
+client = asana.Client.access_token('1/1201719304365594:93eb946e8425cb45865ad7829a527f0c')
 
 result = client.projects.get_projects({'param': 'value', 'param': 'value'}, opt_pretty=True)
