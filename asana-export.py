14953fbb8c2253f18c89a3d19c611cfab877d80d
laccces <en07djc@icloud.com>
2023-07-19 15:36:36 +0100

    Cleaning up old files

--- asana-export.py
+++ asana-export.py
@@ -8,19 +8,18 @@ with open('config.json', 'r') as f:
     config = json.load(f)
 
 PERSONAL_ACCESS_TOKEN = config['api_key']
+WORKSPACE = config['workspace']
+
+client = asana.Client.access_token(PERSONAL_ACCESS_TOKEN)
 
 csv_filename = 'time_tracking_entries.csv'
 csv_headers = ['gid', 'resource_type', 'duration_minutes', 'created_by', 'entered_on']
 
-project_gid = "1203891839848794"
+# Fetch all projects
+result = client.projects.get_projects({'workspace': WORKSPACE, 'archived': 'false'}, opt_pretty=True)
 
-# get a project
-
-client = asana.Client.access_token(PERSONAL_ACCESS_TOKEN)
-
-result = client.tasks.get_tasks({'project':'1203891839848794'}, opt_pretty=True, headers={'asana-enable':'new_goal_memberships'})
-
-gids = [task['gid'] for task in result]
+# Create a list of project gids
+project_gids = [project['gid'] for project in result]
 
 with open(csv_filename, 'w', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=csv_headers)
@@ -28,27 +27,35 @@ with open(csv_filename, 'w', newline='') as file:
     # Write the header
     writer.writeheader()
 
-    # For each gid
-    for gid in gids:
-        url = "https://app.asana.com/api/1.0/tasks/" + gid + "/time_tracking_entries"
+    # Loop through each project gid
+    for project_gid in project_gids:
+        # Fetch all tasks for the given project
+        tasks = client.tasks.get_tasks({'project': project_gid}, opt_pretty=True)
 
-        headers = {
-            "accept": "application/json",
-            "authorization": "Bearer 1/1201719304365594:93eb946e8425cb45865ad7829a527f0c"
-        }
+        # Extract task gids
+        task_gids = [task['gid'] for task in tasks]
 
-        # Get the response
-        response = requests.get(url, headers=headers)
+        # For each task gid
+        for task_gid in task_gids:
+            url = "https://app.asana.com/api/1.0/tasks/" + task_gid + "/time_tracking_entries"
 
-        # Parse the response text into a dictionary
-        response_data = json.loads(response.text)
+            headers = {
+                "accept": "application/json",
+                "authorization": "Bearer " + PERSONAL_ACCESS_TOKEN
+            }
 
-        # Extract the actual data from the response (which is a list)
-        data_list = response_data.get('data', [])
+            # Get the response
+            response = requests.get(url, headers=headers)
 
-        # Write each dictionary in the list to the CSV file
-        for data in data_list:
-            # Extract the 'name' from the 'created_by' dictionary
-            created_by_name = data['created_by'].get('name')
-            data['created_by'] = created_by_name
-            writer.writerow(data)
+            # Parse the response text into a dictionary
+            response_data = json.loads(response.text)
+
+            # Extract the actual data from the response (which is a list)
+            data_list = response_data.get('data', [])
+
+            # Write each dictionary in the list to the CSV file
+            for data in data_list:
+                # Extract the 'name' from the 'created_by' dictionary
+                created_by_name = data['created_by'].get('name')
+                data['created_by'] = created_by_name
+                writer.writerow(data)
