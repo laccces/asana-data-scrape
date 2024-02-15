fab1d0d7fdeaa19d0ea8dc7d4148d6244e0073c9
laccces <en07djc@icloud.com>
2024-02-15 10:35:15 +0000

    Modified scripts to accept multiple gids and batch archive

--- archive-check.py
+++ archive-check.py
@@ -17,8 +17,9 @@ headers = {
     "authorization": f"Bearer {PERSONAL_ACCESS_TOKEN}"
 }
 
-project_gid = "1206430662353429"
+project_gid_list = ["1205116582654603", "1205116338200473", "1205116582639456", "1205116582729758"]
 
-result = client.projects.get_project(project_gid, {'opt_fields': 'name,team.name,archived'}, opt_pretty=True)
-
-print(result)
+for project_gid in project_gid_list:
+    result = client.projects.get_project(project_gid, {'opt_fields': 'name,team.name,archived'}, opt_pretty=True)
+    
+    print(result)
