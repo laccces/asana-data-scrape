964c5c5a08fa9b82731480311a31e7da8f78a920
laccces <en07djc@icloud.com>
2024-02-22 10:03:59 +0000

    Added functionality to provide multiple gids

--- archive-project.py
+++ archive-project.py
@@ -19,23 +19,24 @@ api_client = asana.ApiClient(configuration)
 # create an instance of the API class
 projects_api_instance = asana.ProjectsApi(api_client)
 
-project_gid = "1205116582532611"  # str | Globally unique identifier for the project.
+project_gids = ["1205116338164165", "1205116338251102"]  # str | Globally unique identifier for the project.
 
 # Get the project details before updating
-try:
-    project_details_before = projects_api_instance.get_project(project_gid, opts={'opt_fields': 'name,archived'})
-    print("Project Details Before Update:")
-    pprint(project_details_before)
-except ApiException as e:
-    print("Exception when calling ProjectsApi->get_project: %s\n" % e)
+for project_gid in project_gids:
+    try:
+        project_details_before = projects_api_instance.get_project(project_gid, opts={'opt_fields': 'name,archived'})
+        print("Project Details Before Update:")
+        pprint(project_details_before)
+    except ApiException as e:
+        print("Exception when calling ProjectsApi->get_project: %s\n" % e)
 
-# Update the project
-body = {"data": {"archived": True}}  # dict | The updated fields for the project.
-opts = {'opt_fields': 'name, archived'}
-try:
-    # Update a project
-    api_response = projects_api_instance.update_project(body, project_gid, opts)
-    print("Project Details After Update:")
-    pprint(api_response)
-except ApiException as e:
-    print("Exception when calling ProjectsApi->update_project: %s\n" % e)
+    # Update the project
+    body = {"data": {"archived": True}}  # dict | The updated fields for the project.
+    opts = {'opt_fields': 'name, archived'}
+    try:
+        # Update a project
+        api_response = projects_api_instance.update_project(body, project_gid, opts)
+        print("Project Details After Update:")
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling ProjectsApi->update_project: %s\n" % e)
