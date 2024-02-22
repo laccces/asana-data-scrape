ab786cfc0c80a0429ba10365550b747e12db8067
laccces <en07djc@icloud.com>
2024-02-22 09:27:22 +0000

    Code now working. Needs refinement.

--- get-projects.py
+++ get-projects.py
@@ -17,15 +17,28 @@ api_client = asana.ApiClient(configuration)
 # create an instance of the API class
 projects_api_instance = asana.ProjectsApi(api_client)
 opts = {
-    'workspace': WORKSPACE, 
-    'archived': False, 
-    'opt_fields': "name,owner,team,modified_at"
+    'workspace': WORKSPACE,
+    'archived': False,
+    'opt_fields': "name,owner,team.name,modified_at"
 }
 
 try:
     # Get multiple projects
     api_response = projects_api_instance.get_projects(opts)
-    for data in api_response:
-        pprint(data)
+    
+    # Write data to CSV file
+    with open('project-export.csv', 'w', newline='') as csvfile:
+        fieldnames = ['name', 'owner', 'team', 'modified_at']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        writer.writeheader()
+        for data in api_response:
+            owner_gid = data['owner']['gid'] if data.get('owner') else ''
+            team_gid = data['team']['gid'] if data.get('team') else ''
+            writer.writerow({'name': data['name'],
+                             'owner': owner_gid,
+                             'team': team_gid,
+                             'modified_at': data['modified_at']})
+    print("Export to CSV successful.")
 except ApiException as e:
     print("Exception when calling ProjectsApi->get_projects: %s\n" % e)
